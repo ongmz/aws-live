46381b353fd44384b8d6e111b972b4e4b585f3e7
ongmz <138643077+ongmz@users.noreply.github.com>
2023-08-28 10:48:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cbyj09g16obr.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ongmingzheng-employee"
+customregion = "us-east-1"
+
